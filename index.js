9171980667037c2fe1cc60e224a4e5c7b50549c8
Akshit Vijay <akshit.vijay@mindtickle.com>
2019-02-05 21:48:47 +0530

    jira/ORCHBUGS-276 Handle Historical TImezone names

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-module.exports = require('./timezones.json');
+var oldTimezonesList = require('./oldTimezones.json');
+var timezonesList = require('./timezones.json');
+
+var allTimezonesList = oldTimezones.concat(timezonesList);
+
+module.exports = {
+    oldTimezonesList: oldTimezonesList,
+    timezonesList: timezonesList,
+    allTimezonesList: allTimezonesList
+}
